ed737b3e5f7e0932f499433d65adf839fff0cdb5
Oleg <yastreb92@inbox.ru>
2017-07-08 17:05:48 +0300

    count-block

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function(){
   $(".promo-slider__wrap").owlCarousel({
       loop: true,
       margin: 1,
@@ -39,3 +39,32 @@ $(".how-we-work__content_row-title").click(function () {
        return false;
   });
 /*spoiler*/
+
+/*count-block*/
+var show = true;
+var countbox = "#counts";
+$(window).on("scroll load resize", function(){
+
+    if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+
+    var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+    var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+
+    var w_height = $(window).height();        // Высота окна браузера
+    var d_height = $(document).height();      // Высота всего документа
+
+    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+
+    if(w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+        $(".spincrement").spincrement({
+            thousandSeparator: "",
+            duration: 1500
+        });
+
+        show = false;
+    }
+});
+/*count-block*/
+
+
+});
